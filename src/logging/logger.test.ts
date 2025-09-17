401bf918a5f155590857a764bbca0405f22301c7
Timothy Lowrimore <tlowrimore@heroku.com>
2025-09-17 16:52:16 -0600

    initial commit of logger implementation

---
+++ src/logging/logger.test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import { DefaultLogger, Logger } from './logger';
+
+describe('DefaultLogger', () => {
+  it('implements Logger', () => {});
+
+  it('has a default log-level of Info', () => {});
+
+  it('always logs its context in addition to the log msg and meta', () => {});
+});
---
+++ src/logging/logger.ts
@@ -0,0 +1,108 @@
+// Ordered from least to most verbose.
+export enum LogLevel {
+  Silent,
+  Fatal,
+  Error,
+  Warn,
+  Info,
+  Debug,
+  Trace,
+}
+
+export enum LogDestination {
+  StdOut = 'log',
+  StdErr = 'error',
+}
+
+export type LogMeta = Record<string, unknown>;
+
+export interface LoggerOptions {
+  level?: LogLevel;
+  redactPaths?: string[];
+  destination?: LogDestination;
+}
+
+export interface Logger {
+  child(binding: LogMeta): Logger;
+  level: LogLevel;
+  fatal(msg: string, meta?: LogMeta): void;
+  error(msg: string, meta?: LogMeta): void;
+  warn(msg: string, meta?: LogMeta): void;
+  info(msg: string, meta?: LogMeta): void;
+  debug(msg: string, meta?: LogMeta): void;
+  trace(msg: string, meta?: LogMeta): void;
+}
+
+export class DefaultLogger implements Logger {
+  // Defaults for this implementation of Logger
+  static readonly defaultLevel = LogLevel.Info;
+  static readonly defaultDestination = LogDestination.StdOut;
+  static readonly defaultRedactPaths = [];
+
+  private readonly context: LogMeta;
+  private readonly writeMessage;
+
+  public readonly destination: LogDestination;
+  public redactPaths: string[];
+  public level: LogLevel;
+
+  constructor(context: LogMeta, options?: LoggerOptions) {
+    this.context = context;
+    this.level = options?.level || DefaultLogger.defaultLevel;
+    this.destination = options?.destination || DefaultLogger.defaultDestination;
+    this.redactPaths = options?.redactPaths || DefaultLogger.defaultRedactPaths;
+    this.writeMessage = console[this.destination];
+  }
+
+  child(context: LogMeta): Logger {
+    const childContext = { ...this.context, ...context };
+    const childOptions = {
+      level: this.level,
+      redactPaths: this.redactPaths,
+      destination: this.destination,
+    };
+
+    return new DefaultLogger(childContext, childOptions);
+  }
+
+  fatal(msg: string, meta?: LogMeta): void {
+    this.log(LogLevel.Fatal, msg, meta);
+  }
+
+  error(msg: string, meta?: LogMeta): void {
+    this.log(LogLevel.Error, msg, meta);
+  }
+
+  warn(msg: string, meta?: LogMeta): void {
+    this.log(LogLevel.Warn, msg, meta);
+  }
+
+  info(msg: string, meta?: LogMeta): void {
+    this.log(LogLevel.Info, msg, meta);
+  }
+
+  debug(msg: string, meta?: LogMeta): void {
+    this.log(LogLevel.Debug, msg, meta);
+  }
+
+  trace(msg: string, meta?: LogMeta): void {
+    this.log(LogLevel.Trace, msg, meta);
+  }
+
+  private log(level: LogLevel, msg: string, meta?: LogMeta): void {
+    if (level === LogLevel.Silent) {
+      return;
+    }
+
+    if (level <= this.level) {
+      const message = {
+        message: msg,
+        level: LogLevel[level],
+        ...this.context,
+        ...meta,
+      };
+
+      this.writeMessage(message);
+    }
+  }
+}
